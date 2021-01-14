f2db966f86be05a9e56e1fab7a214b0ee88c9280
yashk2000 <yashsja@gmail.com>
2021-01-14 11:45:07 +0530

    Fix age issue and new blog

--- src/pages/me/AboutMe.js
+++ src/pages/me/AboutMe.js
@@ -188,7 +188,7 @@ const calculateAge = birthday => {
   // birthday is a date
   const ageDifMs = Date.now() - birthday.getTime();
   const ageDate = new Date(ageDifMs); // miliseconds from epoch
-  return Math.abs(ageDate.getUTCFullYear() - 1970) + 1;
+  return Math.abs(ageDate.getUTCFullYear() - 1970);
 };
 
 const AboutMe = () => (
